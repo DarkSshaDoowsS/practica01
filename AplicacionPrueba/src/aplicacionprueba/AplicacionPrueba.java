48b11ad8148cc8be775e923646f6393e721e1396
CESDE <CESDE@PC-24>
2018-10-10 09:13:38 -0500

    se ha terminado de agregar los beans principales

--- AplicacionPrueba/src/aplicacionprueba/AplicacionPrueba.java
+++ AplicacionPrueba/src/aplicacionprueba/AplicacionPrueba.java
@@ -17,6 +17,8 @@ public class AplicacionPrueba {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        // modificado por Alexis
+        
         // modificando el codigo fuente de mi aplicacion
         System.out.println("Hola Mundo");
     }
